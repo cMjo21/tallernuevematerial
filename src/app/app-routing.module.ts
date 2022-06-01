f38e0d70f944b3344635d701424e8339c6a43a1e
johann Arley Cruz Méndez <90436414+cMjo21@users.noreply.github.com>
2022-05-31 22:34:36 -0500

    tallernueve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { FormularioComponent } from './componente/formulario/formulario.component';
+import { CalculadoraComponent } from './componente/calculadora/calculadora.component';
+import { PresentacionComponent } from './componente/presentacion/presentacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'calculadora', component: CalculadoraComponent},
+  {path:'formulario', component: FormularioComponent},
+  {path:'inicio', component: InicioComponent},
+  {path:'presentacion', component: PresentacionComponent },
+  {path: '', component: InicioComponent }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
